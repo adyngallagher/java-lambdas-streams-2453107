1396e2f9a190cb6a452363b6de10462de54cf67f
Adyn Gallagher <adyn.r.gallagher@gmail.com>
2023-10-30 12:02:58 -0400

    chapter three follow along

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,17 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Main {
 
   public static void main(String[] args) {
 
     Integer[] scores = new Integer[]{80, 66, 73, 92, 43};
+    Stream<Integer> scoresStream = Arrays.stream(scores);
 
     List<String> shoppingList = new ArrayList<>();
     shoppingList.add("coffee");
@@ -15,6 +19,18 @@ public class Main {
     shoppingList.add("pineapple");
     shoppingList.add("milk");
     shoppingList.add("pasta");
+
+    List<String> sortedShoppingList = shoppingList.stream()
+            .sorted()
+            .map(item -> item.toUpperCase())
+            .filter(item -> item.startsWith("P"))
+            .collect(Collectors.toList());
+
+    System.out.println(sortedShoppingList);
+    System.out.println(shoppingList);
+
+    Stream<String> lettersStream = Stream.of("a", "b", "c");
+
 
   }
 }
